00176aa572463ffca43a4347d3e3275c0869711c
Edson Batista <126781804+edsonbnasc@users.noreply.github.com>
2023-06-05 10:45:53 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+
+
+
+
+
+
+
+
+/*const superrobo = document.querySelector("#dino")
+arrow function no java atualizado ()=> mesmo que function() 
+superrobo.addEventListener("click",function(evento){
+  
+   console.log(evento)
+   /*console.log("click no Galvatron")
+}
+)
+/*console.log("Bom dia a todos")
+/*document.querySelector("#dino").addEventListener("click",dizOi)/*escultador fica aguardando a pagina o que o ususario faz */
+/*criar uma função para fazer alguma coisa */
+/*function dizOi(nome){
+    console.log(nome)
+    console.log("Estou aprendendo JS " +nome)
+    console.log("Bem vindo ao Transformer 2023")
+}
+dizOi("Edson")*/
